d6a02774d662352cde0d338eabf4a9b51f021654
Oleksandr Prykotenko <aprykotenko@gmail.com>
2020-06-16 18:00:39 -0700

    33

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,20 +2,8 @@ const router = require("express").Router()
 const Workout = require("../models/exercise")
 
 
-router.post("/api/workouts", (req,res) => {
+router.post("/api/workouts", (req, res) =>{
     Workout.create({})
-    .then(data => {
-        res.json(data)
-    }).catch (err => {
-        res.json(err)
-    })
-})
-
-router.put("/api/workouts/:id", ({body, params}, res)=> {
-    Workout.findByIdAndUpdate(
-        params.id,
-        {$push: {exercise: body}}
-    )
     .then(data => {
         res.json(data)
     }).catch(err => {
@@ -23,7 +11,28 @@ router.put("/api/workouts/:id", ({body, params}, res)=> {
     })
 })
 
-router.get("/api/workouts/range", (req, res)=> {
+router.put("/api/workouts/:id", ({body, params}, res) =>{
+    Workout.findByIdAndUpdate(
+        params.id, 
+        {$push: {exercises: body}}       
+    )
+    .then(data => {
+        res.json(data)
+    }).catch(err => {
+        res.json(err)
+    })
+} )
+
+router.get("/api/workouts", (req, res)=> {
+    Workout.find()
+    .then(data => {
+        res.json(data)
+    }).catch(err => {
+        res.json(err)
+    })
+})
+
+router.get("/api/workouts/range", (req, res)=>{
     Workout.find({}).limit(7)
     .then(data => {
         res.json(data)
@@ -33,13 +42,5 @@ router.get("/api/workouts/range", (req, res)=> {
 })
 
 
-router.get("/api/workouts", (req,res) => {
-    Workout.find()
-    .then(data => {
-        res.json(data)
-    }).catch (err => {
-        res.json(err)
-    })
-})
 
-module.exports = router;
+module.exports = router
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,18 +1,18 @@
-const router = require ("express").Router()
+const router = require("express").Router()
 const path = require("path");
 
 // Routes
-
-router.get("/", function (req,res){
-    res.sendFile(path.join(__dirname, "../path/index.html"));
+// =============================================================
+router.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-router.get("/exercise", function (req,res){
+  router.get("/exercise", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
-});
+  });
 
-router.get("/stats", function (req, res){
+  router.get("/stats", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
-});
+  });
 
-module.exports = router
+  module.exports = router
